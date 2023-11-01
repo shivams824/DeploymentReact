092619071d87f18a640679d7074831e0ec2bef83
shivams824 <shivam.singh@wonderbiz.in>
2023-11-01 18:05:21 +0530

    Referred Updates in routing

--- src/App.tsx
+++ src/App.tsx
@@ -1,28 +1,28 @@
-
-import './App.css';
-import 'ag-grid-community/styles/ag-grid.css';
-import LoginPage from './Components/Login/LoginPage';
-import Home from './Components/Home/Home';
-import { BrowserRouter, Route, Routes } from 'react-router-dom';
-import View from './Components/View/View';
-import ProfileManagement from './Components/ProfileManagement/ProfileManagement';
+import "./App.css";
+import "ag-grid-community/styles/ag-grid.css";
+import LoginPage from "./Components/Login/LoginPage";
+import Home from "./Components/Home/Home";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import View from "./Components/View/View";
+import ProfileManagement from "./Components/ProfileManagement/ProfileManagement";
 // import { Login } from './Components/trying/Login';
-import AssetDetail from './Components/AssetDetail/AssetDetail';
+import AssetDetail from "./Components/AssetDetail/AssetDetail";
 // import OperatorHome from './Components/Home/OperatorHome';
 
-
-function App() {
+const App = () => {
   return (
-    <div className='App'>
-      <Routes>
-      <Route path="/" element={<LoginPage />} />
-        <Route path="/Home" element={<Home role="role" />} />
-        <Route path="view" element={<View />} />
-        <Route path="/asset/:type" Component={AssetDetail} />
+    <div className="App">
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<LoginPage />} />
+          <Route path="/Home" element={<Home role="role" />} />
+          <Route path="view" element={<View />} />
+          <Route path="/asset/:type" Component={AssetDetail} />
           <Route path="/manage-profile" element={<ProfileManagement />} />
-      </Routes>
+        </Routes>
+      </BrowserRouter>
     </div>
   );
-}
+};
 
 export default App;
