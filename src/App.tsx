c4bd437c793471225d3b0c2ec7070d19dc35b55e
shivams824 <shivam.singh@wonderbiz.in>
2023-11-02 10:41:12 +0530

    Trying View

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,7 @@ import "./App.css";
 import "ag-grid-community/styles/ag-grid.css";
 import LoginPage from "./Components/Login/LoginPage";
 import Home from "./Components/Home/Home";
-import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { BrowserRouter, Route, Routes,  } from "react-router-dom";
 import View from "./Components/View/View";
 import ProfileManagement from "./Components/ProfileManagement/ProfileManagement";
 // import { Login } from './Components/trying/Login';
@@ -14,8 +14,8 @@ const App = () => {
     <div className="App">
         <Routes>
           {/* <Route path="/" element={<LoginPage />} /> */}
-          <Route path="/" element={<Home role="role" />} />
-          <Route path="view" element={<View />} />
+          {/* <Route path="/" element={<Home role="role" />} /> */}
+          <Route path="/" element={<View />} />
           <Route path="/asset/:type" Component={AssetDetail} />
           <Route path="/manage-profile" element={<ProfileManagement />} />
         </Routes>
